971c17e588e4e799358185ca9fda7fa5c4e52e61
CaoHuy26 <huyc197@gmail.com>
2020-05-14 20:37:56 +0700

    Send email template and save it to db

--- sendEmail.php
+++ sendEmail.php
@@ -30,37 +30,64 @@
   </form>
 
   <?php
+    include('./connect.php');
     if (isset($_POST['send'])) {
       $emailInput = $_POST['email'];
       $username = $_POST['username'];
       $msv = $_POST['msv'];
+      date_default_timezone_set('Asia/Ho_Chi_Minh');
+      $dateTime = date("Y-m-d H:i:s");
 
-      $mail = new PHPMailer(true);
+      // Save to db
+      $query = "INSERT INTO email(username, msv, email, datetime)
+        VALUES('$username', '$msv', '$emailInput', '$dateTime')";
+      $runQuery = mysqli_query($conn, $query);
+      if ($runQuery) {
+        $mail = new PHPMailer(true);
 
-      try {
-        //Server settings
-        $mail->SMTPDebug = 2;
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'huyc197@gmail.com'; // Email
-        $mail->Password   = '';                  // Password
-        $mail->SMTPSecure = 'tls';
-        $mail->Port       = 587;
+        // Test template
+        $variables = array();
+        $variables['emailInput'] = $emailInput;
+        $variables['username'] = $username;
+        $variables['msv'] = $msv;
+        $variables['dateTime'] = $dateTime;
+        $emailTemplate = file_get_contents('email-template.html');
+        foreach ($variables as $key => $value) {
+          $emailTemplate = str_replace('{{ '.$key.' }}', $value, $emailTemplate);
+        }
+        echo $emailTemplate;
+
+        try {
+          //Server settings
+          $mail->SMTPDebug = 2;
+          $mail->isSMTP();
+          $mail->CharSet  = "utf-8";
+          $mail->Host = 'smtp.gmail.com';
+          $mail->SMTPAuth = true;
+          $mail->Username = 'huyc197@gmail.com'; // Email
+          $mail->Password = ''; // Password
+          $mail->SMTPSecure = 'tls';
+          $mail->Port = 587;
+      
+          //Recipients
+          $mail->setFrom('huyc197@gmail.com');
+          $mail->addAddress($emailInput);
     
-        //Recipients
-        $mail->setFrom('huyc197@gmail.com');
-        $mail->addAddress($emailInput);
-  
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Dang nhap thanh cong';
-        $mail->Body    = 'Họ tên: ' .$username. 'Mã sinh viên: ' .$msv. 'Thời gian: ' .date("Y/m/d");
-    
-        $mail->Send();
-        echo 'Message has been sent';
-      } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+          // Content
+          $mail->isHTML(true);
+          $mail->Subject = 'Đăng nhập thành công';
+          // $mail->Body = 'Họ tên: ' .$username. 'Mã sinh viên: ' .$msv. 'Thời gian: ' .$dateTime;
+          $mail->Body = $emailTemplate;
+
+          $mail->Send();
+          echo 'Message has been sent';
+        } catch (Exception $e) {
+          echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+      }
+      else {
+        echo "Lỗi";
+        echo $query;
       }
     }
   ?>
