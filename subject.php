aa4b00ae1112ed2f9365139dccb533d2c232af64
CaoHuy26 <huyc197@gmail.com>
2020-05-24 02:25:01 +0700

    Get value from checkbox

---
+++ subject.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form method="POST">
+    Name: <input type="text" name="name"/>
+    <br>
+    ID: <input type="text" name="id"/>
+    <br>
+    Class: <input type="text" name="class"/>
+    <br>
+    Subject:
+    <br>
+    PHP: <input type="checkbox" name="subject[]" value="PHP"/>
+    SQL: <input type="checkbox" name="subject[]" value="SQL"/>
+    Photoshop: <input type="checkbox" name="subject[]" value="Photoshop"/>
+    <br>
+    Linux: <input type="checkbox" name="subject[]" value="Linux"/>
+    Java: <input type="checkbox" name="subject[]" value="Java"/>
+    Android: <input type="checkbox" name="subject[]" value="Android"/>
+    <br>
+    C#: <input type="checkbox" name="subject[]" value="C#"/>
+    ASP.NET: <input type="checkbox" name="subject[]" value="ASP.NET"/>
+    Do hoa: <input type="checkbox" name="subject[]" value="Do hoa"/>
+    <br>
+    <input type="submit" value="Submit" name="submit"/>
+  </form>
+
+  <?php
+    include('./connect.php');
+    if (isset($_POST['submit'])) {
+      $name = $_POST['name'];
+      $id = $_POST['id'];
+      $class = $_POST['class'];
+      $subjects = $_POST['subject'];
+      
+      $subjectStr = '';
+
+      foreach($subjects as $subject) {
+        $subjectStr .= $subject . ' ';
+      }
+
+      $query = 
+      "INSERT INTO subject(name, id, class, subject)
+       VALUES('$name', '$id', '$class', '$subjectStr')";
+      $run = mysqli_query($conn, $query);
+      if ($run) {
+        echo "<script>alert('Done')</script>";
+      }
+    }
+  ?>
+</body>
+</html>
